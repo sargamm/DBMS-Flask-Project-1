74faf8fd2393383d847391d4ada56ae48c6f3e80
crypticguy <vasu18322@iiitd.ac.in>
2020-04-23 13:46:11 +0530

    Just added simple routes

--- app.py
+++ app.py
@@ -4,3 +4,23 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World'
+
+@app.route('/login')
+def login():
+    return 'Login'
+
+@app.route('/signup')
+def signup():
+    return 'Signup'
+
+@app.route('/addRecord')
+def addRecord():
+    return 'record'
+
+@app.route('/deleteRecord')
+def deleteRecord():
+    return 'delete record'
+
+@app.route('/viewRecords')
+def viewRecords():
+    return 'view records'
